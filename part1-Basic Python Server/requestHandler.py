1c1a839e44c79791ddc2d983a00a8682a371e0ad
dareenhanash <dareenhanash1988@gmail.com>
2018-08-02 14:28:52 +0300

    part

--- part1-Basic Python Server/requestHandler.py
+++ part1-Basic Python Server/requestHandler.py
@@ -7,11 +7,12 @@ class myHandler(BaseHTTPRequestHandler):
 	#This will handle GET requests
 	def do_GET(self):
 		# Add the outgoing status here
-		
+		self.send_response(200)
 		# You will need to change this if you are sending something
   		# other than plain text, like JSON or HTML.
 		self.send_header('Content-type','text/html')
 		self.end_headers()
+		self.wfile.write("hello world")
 
 		# Send the "Hello World" message here 
 		
